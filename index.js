3fc0c10066f629c3ca7dd0d1f942bb32be766208
JeremyPerezGit <jeremy.perez13@hotmail.fr>
2024-10-15 11:16:38 +0200

    version 1.2

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import chalk from "chalk";
 
-console.log(chalk.blue("Jeremy"));
-console.log(chalk.red("Maxence"));
-console.log(chalk.green("Flavien"));
-console.log(chalk.yellow("Vincent"));
+console.log(
+  chalk.blue("Jeremy") +
+    chalk.red(" Maxence") +
+    chalk.green(" Flavien") +
+    chalk.yellow(" Vincent")
+);
